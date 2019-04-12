40263eda6d47d55f24542c2c0719fbad56b94f8d
Kenneth Redillas <kcredillas@mix.wvu.edu>
2019-04-12 14:02:06 -0400

    added error handlers

--- access.py
+++ access.py
@@ -1,10 +1,12 @@
 import cmd, sys
+import re
+
 
 #list of valid commands to use in the test case file
 validCommands = ['friendadd', 'viewby', 'logout', 'listadd', 'friendlist', 'postpicture', 'chlst', 'chmod', 'chown', 'readcomments', 'writecomments', 'end']
 
 def main():
-    print ('Argument List:', str(sys.argv))
+    #print ('Argument List:', str(sys.argv))
 
     #Error Handling
     if (len(sys.argv) == 2):
@@ -15,14 +17,31 @@ def main():
         try:
             fObj = open(fileName, 'r')
             badCommands = list()
+            goodCommands = list()
             for line in fObj:
                 command = line.split(' ')[0]
                 if command.strip() not in validCommands:
                     badCommands.append(command)
-                    
-                
+                else:
+                    #this is a valid command!
+                    goodCommands.append(line.strip())
+            fObj.close()
+
+            if (len(badCommands) > 0):
+                print("You queried some invalid commands!")
+                print("List of invalid commands: " + badCommands)
+                return -1
+            
+            #if file is valid, continue checking
+            if goodCommands[0].split()[0] != "friendadd":
+                print("First command must be \"friendadd\".")
+                #TODO: report error to audit log file
+                return -1
+            
         except IOError:
             print("File not found.")
+            #TODO: report file error to audit log file
+        
         
     else:
         print("Error: Need to have a test case file as an argument, where file is the one argument.")
