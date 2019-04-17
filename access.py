b27c685b7c76b0d990bed2946bb1e1b7cc57f328
Kenneth Redillas <kcredillas@mix.wvu.edu>
2019-04-17 19:39:17 -0400

    tester branch: progress good so far

--- access.py
+++ access.py
@@ -14,30 +14,20 @@ friend_txt_list = list()
 listOfFriendLists = dict()
 
 # dictionary that keeps track of picture associated to the owner and the pictures posted
-picture_traking = dict()
+picture_tracking = dict()
+
+#list to keep track of current picture objects
+picture_objects = list()
 
 # boolean value to keep track if someone is viewing or not
 is_viewing = False
 
-#track who is viewing
+# track who is viewing
 who_is_viewing = []
 
 
 class AccessControlList(object):
     pass
-
-
-class Person(object):
-    pass
-    """
-    isFriend = False #default value of a friend
-    def __init__(self, name, AccessControlList):
-        self.name = name
-        self.AccessControlList = AccessControlList
-
-    def is_Friend(self):
-        return self.isFriend
-    """
 
 
 class Admin(object):
@@ -57,6 +47,19 @@ class Admin(object):
         cls.online_status = online_status
 
 
+class Picture(object):
+    name = ''
+    picture_group = None
+    owner = ''
+    others = None #TODO: difference of all friends and pricture_group
+    permissions = {owner:'rw', picture_group:'--', others: '--'}
+    
+    def __init__(self, name, owner, permissions):
+        self.name = name
+        self.owner = owner
+        self.permissions = permissions
+
+
 """
 Function: friendadd
 Description: Creates an instance of a friend profile, not belonging to any list by default
@@ -65,12 +68,12 @@ Description: Creates an instance of a friend profile, not belonging to any list 
 
 def friendadd(friendname):
     if friendname in friend_txt_list:
-        print("Error: Username " + "\'" + friendname + "\' already exists.")
-        # TODO: Report to audit.txt
-        exit
+        log_message("Friendadd failed: Username " + "\'" +
+                    friendname + "\' already exists.")
+        return
     #instance_friend = Friend(friendname)
     friend_txt_list.append(friendname)
-    
+    log_message("Friend " + friendname + " added")
     with open('friends.txt', 'a+') as fObj:
         fObj.write(friendname + '\n')
 
@@ -85,26 +88,26 @@ def viewby(friendname):
     global who_is_viewing
 
     if friendname not in friend_txt_list:
-        print("Error: Username " + "\'" + friendname +
-              "\' is not in friends.txt file.")
+        log_message("View failed: Username " + "\'" + friendname +
+                    "\' is not in friends.txt file.")
         return
-        #sys.exit(1)
+        # sys.exit(1)
 
     if (is_viewing == True):
-        print("Error: Username " + "\'" + who_is_viewing[0] +
-              "\' is viewing right now. " + "\'" + friendname +
-              "\' will not execute any commands then.")
+        log_message("View failed: Username " + "\'" + who_is_viewing[0] +
+                    "\' is viewing right now. " + "\'" + friendname +
+                    "\' will not execute any commands then.")
         return
-        #sys.exit(1)
+        # sys.exit(1)
 
     is_viewing = True
     who_is_viewing.append(friendname)
     if friendname == friend_txt_list[0]:
-        print("Supreme Leader of MyFacebook " + "\'" + friendname +
-              "\' is viewing his/her profile.")
+        log_message("Admin user " + "\'" + friendname +
+                    "\' is viewing his/her profile.")
     else:
-        print("Friend " + "\'" + friendname +
-              "\' is viewing the profile.")
+        log_message("Friend " + "\'" + friendname +
+                    "\' is viewing the profile.")
 
 
 def logout():
@@ -116,53 +119,81 @@ def logout():
         pass
     else:
         is_viewing = False
-        print(who_is_viewing[0] + " is no longer viewing profile.")
+        log_message("Friend " + who_is_viewing[0] + " has logged out.")
         who_is_viewing.pop()
+
 
 def listadd(listname):
     global who_is_viewing
     if who_is_viewing[0] != friend_txt_list[0]:
-        print("Error: Username " + "\'" + who_is_viewing[0] +
-              "\' can't  execute listadd because he/she is not the Supreme Leader of MyFacebook " + friend_txt_list[0])
+        log_message("Listadd failed: Username " + "\'" + who_is_viewing[0] +
+                    "\' can't  execute listadd because he/she is not the Supreme Leader of MyFacebook " + friend_txt_list[0])
         return
     if listname == "nil":
-        print("Error: Can't create a list called 'nil'")
+        log_message("Listadd failed: Can't create a list called 'nil'")
         return
     if listname in listOfFriendLists:
-        print("Error: Can't instantiate a list that already exists")
+        log_message("Listadd: Can't instantiate a list that already exists")
         return
     listOfFriendLists[listname] = list()
+    log_message("List " + listname + " created")
     print(str(listOfFriendLists))
-    #4/16/2019 7PM
+    # 4/16/2019 7PM
 
-
-    
-    
 
 def friendlist(friendname, listname):
     global who_is_viewing
     if friendname not in friend_txt_list:
-        print("Error: Username " + "\'" + friendname + "\' does not exist")
+        log_message("Error: Username " + "\'" +
+                    friendname + "\' does not exist")
         return
     if listname not in listOfFriendLists:
-        print("Error: Listname " + "\'" + listname + "\' does not exist")
+        log_message("Error: Listname " + "\'" + listname + "\' does not exist")
         return
     if who_is_viewing[0] != friend_txt_list[0]:
-        print("Error: Username " + "'" + who_is_viewing[0] + "' ain't an admin.")
+        log_message("Error: Username " + "'" +
+                    who_is_viewing[0] + "' ain't an admin.")
         return
     temp = listOfFriendLists.get(listname)
     temp.append(friendname)
-    
+    log_message("Friend " + friendname + " added to list " + listname)
     print(str(listOfFriendLists))
 
-def postpicture():
-    pass
 
+def postpicture(picturename):
+    temp = ''
+    global is_viewing
+    if picturename.endswith('.txt') and is_viewing:
+        temp = picturename.split('.txt')[0]
+        if temp in picture_tracking:
+            log_message("Posting picture failed: " + picturename +
+                        " already exists. Must give this picture a different name")
+            return
+        picture_tracking[temp] = who_is_viewing[0]
+        with open(picturename, 'w+') as fObj:
+            fObj.write(temp + '\n')
+        
 
-def chlst():
-    pass
+        log_message("Picture " + picturename + " with owner " +
+                    picture_tracking[temp] + " and default permissions has been posted")
+    else:
+        if not (picturename.endswith('.txt')):
+            log_message(
+                "Posting picture failed: Picture name needs to end with .txt")
+            return
+        if (is_viewing == False):
+            log_message(
+                "Posting picture failed: No one is viewing the profile")
+            return
 
-
+def chlst(picturename, listname):
+    if who_is_viewing == False:
+        log_message("chlst failed: Someone needs to view the profile.")
+        return
+    if listname == "nil":
+        #disassociate
+        pass
+    
 def chmod():
     pass
 
@@ -190,8 +221,8 @@ def switch_case(command_string, opt_arg1, opt_arg2):
         "logout": logout,
         "listadd": lambda: listadd(opt_arg1),
         "friendlist": lambda: friendlist(opt_arg1, opt_arg2),
-        "postpicture": postpicture,
-        "chlst": chlst,
+        "postpicture": lambda: postpicture(opt_arg1),
+        "chlst": lambda: chlst(opt_arg1, opt_arg2),
         "chmod": chmod,
         "chown": chown,
         "readcomments": readcomments,
@@ -200,6 +231,12 @@ def switch_case(command_string, opt_arg1, opt_arg2):
     }
     func = switcher.get(command_string, lambda: "Invalid command")
     func()
+
+
+def log_message(message):
+    print(message)
+    with open('audit.txt', 'a') as audit:
+        audit.write(message + '\n')
 
 
 def main():
@@ -253,9 +290,8 @@ def main():
                         parse = args[1]
                         parse = re.split('[\\s:/]', parse)
                         opt_arg1 = ''.join(parse)
-                        if "viewby" == command_arg and opt_arg1 == friend_txt_list[0]:
-                            print(opt_arg1 + " now has administrator access!")
-                            #TODO: give him priveleges
+                        # if "viewby" == command_arg and opt_arg1 == friend_txt_list[0]:
+                        #     print(opt_arg1 + " now has administrator access!")
 
                     elif len(args) == 3:
                         parse = re.split('[\\s:/]', args[1])
